3017a97193d1f7ef3d561385c0d725ecb4abc1a0
Dark Factory <darkfactory@local>
2026-05-13 13:36:00 +0200

    fix: automated patch [dark-factory]

--- src/requests/sessions.py
+++ src/requests/sessions.py
@@ -606,8 +606,8 @@ class Session(SessionRedirectMixin):
             content. Defaults to ``False``.
         :param verify: (optional) Either a boolean, in which case it controls whether we verify
             the server's TLS certificate, or a string, in which case it must be a path
-            to a CA bundle to use. Defaults to ``True``. When set to
-            ``False``, requests will accept any TLS certificate presented by
+            to a CA bundle to use. If not specified, defaults to the session's verify setting.
+            When set to ``False``, requests will accept any TLS certificate presented by
             the server, and will ignore hostname mismatches and/or expired
             certificates, which will make your application vulnerable to
             man-in-the-middle (MitM) attacks. Setting verify to ``False``
